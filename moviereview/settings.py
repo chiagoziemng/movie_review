cb0c08079cf83d0db7ebce8ac8683d2d68c32d75
chiagoziemNG <chiagoziemjamesng@gmail.com>
2023-05-16 03:28:58 +0100

    Listing movies and adding a news app

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #
     'movieapp',
+    'news',
 ]
 
 MIDDLEWARE = [
--- moviereview/urls.py
+++ moviereview/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movieapp.urls'))
+    path('', include('movieapp.urls')),
+    path('news/', include('news.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
