b9c09175d6406dfe06a889424cbf3645a16f2364
chiagoziemNG <chiagoziemjamesng@gmail.com>
2023-05-16 05:03:15 +0100

    Implemented user signup and login

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     #
     'movieapp',
     'news',
+    'account',
 ]
 
 MIDDLEWARE = [
--- moviereview/urls.py
+++ moviereview/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movieapp.urls')),
     path('news/', include('news.urls')),
+    path('account/', include('account.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
